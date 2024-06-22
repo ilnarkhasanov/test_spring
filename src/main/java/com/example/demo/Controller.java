eb523057ea6f426a8bcdc874127c27869c16d283
ilnarkhasanov <4sitescarp@gmail.com>
2024-06-22 15:36:38 +0300

    init

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @CrossOrigin("*")
+    @GetMapping("/kek")
+    public String index() {
+        return "index";
+    }
+
+}
